a807171f45f5f42583626af9395f0b8b12948488
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2023-05-27 14:53:34 -0300

    atua  3

--- src/main/java/Seguradora.java
+++ src/main/java/Seguradora.java
@@ -1,16 +1,34 @@
 public class Seguradora implements Visitor {
+    public String exibir(Seguro seguro) {
+        return seguro.aceitar(this);
+    }
     @Override
     public String exibirSeguroPredial(SeguroPredial predial) {
-        return null;
+        return "Seguro Predial{" +
+                "contrato=" + predial.getCodigo() +
+                "Nome do predio=" + predial.getNomePredio() +
+                ", Numero de Apartamentos='" + predial.getNumeroApartamento()+ '\'' +
+                ", valor=" + predial.getValorSeguro() +
+                '}';
     }
 
     @Override
     public String exibirSeguroComercial(SeguroComercio comercio) {
-        return null;
+        return "Seguro Predial{" +
+                "contrato=" + comercio.getCodigo() +
+                "Comercio=" +comercio.getNomeComercio() +
+                ",CNPJ='" +comercio.getCnpj()+ '\'' +
+                ",valor=" + comercio.getValorSeguro() +
+                '}';
     }
 
     @Override
     public String exibirSeguroResidencial(SeguroResidencial residencia) {
-        return null;
+        return "Seguro Predial{" +
+                "contrato=" + residencia.getCodigo() +
+                "Comercio=" +residencia.getNome() +
+                ",CNPJ='" +residencia.getCpf()+ '\'' +
+                ",valor=" + residencia.getValorSeguro() +
+                '}';
     }
 }
